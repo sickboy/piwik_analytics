7214a4053741e9902f1b34a6c65339d15d80e9fd
Sickboy <sb@dev-heaven.net>
2012-05-24 14:34:22 +0200

    Fix; duplicate include+filter on Rails 2.x

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,6 @@
 require 'piwik_analytics'
-ActionController::Base.send :include, PiwikAnalytics::PiwikAnalyticsMixin
-ActionController::Base.send :after_filter, :add_piwik_analytics_tracking
+
+unless ActionController::Base.included?(PiwikAnalytics::PiwikAnalyticsMixin)
+  ActionController::Base.send :include, PiwikAnalytics::PiwikAnalyticsMixin
+  ActionController::Base.send :after_filter, :add_piwik_analytics_tracking
+end
