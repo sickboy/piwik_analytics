b7d2e888794754c1ae9781b663e36a6cf5847588
Sickboy <sb@dev-heaven.net>
2012-05-25 11:05:47 +0200

    included not working?

--- rails/init.rb
+++ rails/init.rb
@@ -1,6 +1,6 @@
 require 'piwik_analytics'
 
-unless ActionController::Base.included?(PiwikAnalytics::PiwikAnalyticsMixin)
+unless ActionController::Base.method_defined? :add_piwik_analytics_tracking
   ActionController::Base.send :include, PiwikAnalytics::PiwikAnalyticsMixin
   ActionController::Base.send :after_filter, :add_piwik_analytics_tracking
 end
