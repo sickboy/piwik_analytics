1d4023ca1716568351342fcfef8d9332054d376f
Patrick Roza <roza@six-networks.org>
2012-08-10 13:47:38 +0200

    Format exception

--- lib/piwik_analytics.rb
+++ lib/piwik_analytics.rb
@@ -76,7 +76,7 @@ module PiwikAnalytics
 
     def self.enabled?(format)
       raise PiwikAnalytics::PiwikAnalyticsConfigurationError if id_site.blank? || url.blank?
-      environments.include?(Rails.env) && formats.include?(format.to_sym)
+      environments.include?(Rails.env) && !format.blank? && formats.include?(format.to_sym)
     end
   end
 end
